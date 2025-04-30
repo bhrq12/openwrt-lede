810ea62431ba837646acb5057aa3805081f33f25
bhrq12 <49489405+bhrq12@users.noreply.github.com>
2025-05-01 02:12:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,7 @@
 
 # Add a feed source
 rm feeds.conf.default
+echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
 echo '#src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
 echo 'src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' >>feeds.conf.default
 echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
