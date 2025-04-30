fcbc3b7190ff9fa4f353ad83b262d82672fa6f32
bhrq12 <49489405+bhrq12@users.noreply.github.com>
2025-05-01 02:01:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,10 +15,13 @@
 
 # Add a feed source
 rm feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo '#src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' >>feeds.conf.default
 echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
 echo 'src-git telephony https://github.com/coolsnowwolf/telephony.git' >>feeds.conf.default
+echo '#src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+echo '#src-git oui https://github.com/zhaojh329/oui.git' >>feeds.conf.default
+echo '#src-git video https://github.com/openwrt/video.git' >>feeds.conf.default
+echo '#src-git targets https://github.com/openwrt/targets.git' >>feeds.conf.default
 echo '#src-git oldpackages http://git.openwrt.org/packages.git' >>feeds.conf.default
+echo '#src-link custom /usr/src/openwrt/custom-feed' >>feeds.conf.default
