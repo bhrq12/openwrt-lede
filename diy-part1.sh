ab9f1cb9dfb6bbd87f1e2d0c9ad902181de5491e
bhrq12 <49489405+bhrq12@users.noreply.github.com>
2025-04-30 21:33:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,9 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+echo 'src-git telephony https://github.com/coolsnowwolf/telephony.git' >>feeds.conf.default
+echo 'src-git oldpackages http://git.openwrt.org/packages.git' >>feeds.conf.default
